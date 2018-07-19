5b0dfe995a8e246217652a1a82c0ed1e9c37609c
James Budarz <jimbudarz@gmail.com>
2018-07-18 21:41:06 -0400

    Split analysis into data treatment (DatasetImprover.m and PollutionExploration.Rmd. Also updated questions in README.md

---
+++ DatasetImprover.R
@@ -0,0 +1,91 @@
+## This script loads the nationwide pollution data and adds information that is useful to me:
+library(dplyr)
+library(ggmap)
+library(censusapi)
+
+df = read.csv("pollution_us_2000_2016.csv", header = TRUE)
+
+# Get U.S. census data to extract populations for each city of interest:
+popestimate <- getCensus(name = "pep/population",
+                         vintage = 2016,
+                         vars = c("POP", "GEONAME", "DATE_DESC"),
+                         region = "place:*")
+popestimate$City_State = gsub(' city', '', popestimate$GEONAME)
+
+# Remove any incomplete entries:
+df = df %>% filter(complete.cases(.))
+
+# Remove locations "not in a city":
+df = df %>% filter(!(City == 'Not in a city'))
+df$Date.Local = as.Date(df$Date.Local)
+
+# Average all data down to a city level:
+df = df %>%
+  group_by(City, Date.Local) %>%
+  summarise(
+    State = first(State), County = first(County),
+    NO2.Units = first(NO2.Units), NO2.Mean = mean(NO2.Mean), NO2.AQI = mean(NO2.AQI),
+    O3.Units = first(O3.Units), O3.Mean = mean(O3.Mean), O3.AQI = mean(O3.AQI),
+    SO2.Units = first(SO2.Units), SO2.Mean = mean(SO2.Mean), SO2.AQI = mean(SO2.AQI),
+    CO.Units = first(CO.Units), CO.Mean = mean(CO.Mean), CO.AQI = mean(CO.AQI)
+  )
+
+# Assign each date a month
+df <- df %>%
+  mutate(month = factor(
+    months(Date.Local),
+    levels = c('January','February','March','April','May','June','July',
+               'August','September','October','November','December')
+  )
+  )
+
+# Fetch location (lat, long) information for every city:
+# To limit queries, reduce list into unique set of cities:
+df$City_State = paste(as.character(df$City),as.character(df$State), sep = ', ')
+allCityStates = as.character(distinct(df,City_State)[["City_State"]])
+
+citycoords = geocode(allCityStates, source = "dsk")
+citycoords$City_State = as.character(distinct(df,City_State)[["City_State"]])
+
+# Join the location into main dataset on City_State
+df = inner_join(df, citycoords, by = 'City_State')
+
+# Find number of measurements for each location:
+df %>% group_by(City) %>%
+  summarize(NumberOfMeasurements = n())
+
+# Convert measurements to a common unit:
+
+
+# Assign AQI labels to numeric values:
+df$NO2.AQItext = cut(df$NO2.AQI,
+                     breaks = c(0,50,100,150,200,300,500),
+                     labels = c('Good','Moderate',
+                                'Unhealthy for Sensitive Groups',
+                                'Unhealthy','Very Unhealthy','Hazardous'))
+df$CO.AQItext = cut(df$CO.AQI,
+                    breaks = c(0,50,100,150,200,300,500),
+                    labels = c('Good','Moderate',
+                               'Unhealthy for Sensitive Groups',
+                               'Unhealthy','Very Unhealthy','Hazardous'))
+df$SO2.AQItext = cut(df$SO2.AQI,
+                     breaks = c(0,50,100,150,200,300,500),
+                     labels = c('Good','Moderate',
+                                'Unhealthy for Sensitive Groups',
+                                'Unhealthy','Very Unhealthy','Hazardous'))
+df$O3.AQItext = cut(df$O3.AQI,
+                    breaks = c(0,50,100,150,200,300,500),
+                    labels = c('Good','Moderate',
+                               'Unhealthy for Sensitive Groups',
+                               'Unhealthy','Very Unhealthy','Hazardous'))
+
+
+# Summarize all location data for average measurements per city per day:
+
+
+# Remove strange data before 2001:
+azdata <- azdata %>% filter(year(Date.Local) > 2001)
+
+
+# Save the file at the very end:
+write.csv(df, file = "PreppedPollutionData.csv")
--- README.md
+++ README.md
@@ -5,13 +5,16 @@ An interactive Shiny App to visualize pollution measurements across the U.S.
 
 Are our current laws keeping us safe from harmful pollution? (For splitting data into danger vs safe zone: What are high vs. safe levels? Asthma etc. https://www.epa.gov/criteria-air-pollutants/naaqs-table)
 
-- Which cities and which areas of the country are most polluted? The least? How does city population affect pollution levels?
+- Which cities and which areas of the country are most polluted? The least?
 **(Map controlled by slider selecting year)**
+
+- How does city population (density) affect pollution levels?
+**(Scatter plot of population vs. pollution levels, radio buttons control which pollutants are plotted)**
 
 - How does city a compare to city b?
 **(Box oplots of AQI for up to 5 selectable cities, plus Two-Sample T-Tests to indicate whether cities are significantly different)**
 
-- How does the pollution change over the long term? Over the short term?
+- How does the pollution change over the long term? Over the short term? Do all cities fluctuate similarly?
 **(Time series by selection of city)**
 
 - How do the different pollutants relate to one another? Can the variables be described as independent?
@@ -38,3 +41,5 @@ Speaker maintains good eye contact with the audience and is appropriately animat
 Speaker uses a clear, audible voice. Delivery is poised, controlled, and smooth.
 Length of presentation is within the assigned time limits.
 Easy to follow and informative.
+
+Disclaimer: "This product uses the Census Bureau Data API but is not endorsed or certified by the Census Bureau."
